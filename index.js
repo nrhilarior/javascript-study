050c8e821bee09edc7993efd64891e0256bb4bb5
Ruan Nicollas <nr.hilario.r@gmail.com>
2025-05-12 09:14:10 -0300

    Primeiro Ajuste

---
+++ index.js
@@ -0,0 +1,16 @@
+let myName = 'Jesse Pinkman'; {
+    console.log(myName);
+}// Name for register
+const age = 25; {
+    console.log(age);
+}// Age 
+const roots = ['Angelina Jollie', 'William Wonka', myName]; {
+    console.log(roots);
+}// Familly Base
+const myFamilly = ['Michael Ross', 'Rachel Zane', myName]; {
+    console.log(myFamilly);
+}// Familly
+
+console.log(-----------------------------------);
+
+let ope = 15 * 2 / 2
---
+++ let name = 'Ruan Nicollas';.js
@@ -0,0 +1,10 @@
+//index.js
+let name = 'Ruan Nicollas';
+const myAge = 19;
+const myFamilly = ['José Claudio', 'Esther Schmoegle', 'Ruan Nicollas'];
+const // Variavél pode ser mudada
+let // Variavél não pode ser mudada
+
+console.log(name);
+console.log(myAge);
+console.log(myFamilly);
